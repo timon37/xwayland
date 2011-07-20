568ad0cbebec871fd48c77c933503dd297389a83
Kristian Høgsberg <krh@bitplanet.net>
2011-07-20 08:58:47 -0700

    xwayland: Skip config_init() when in Wayland mode

--- hw/xfree86/common/xf86Init.c
+++ hw/xfree86/common/xf86Init.c
@@ -894,6 +894,11 @@ InitInput(int argc, char **argv)
 
     mieqInit();
 
+    GetEventList(&xf86Events);
+
+    if (xorgWayland)
+	return;
+
     /* Initialize all configured input devices */
     for (pInfo = xf86ConfigLayout.inputs; pInfo && *pInfo; pInfo++) {
         (*pInfo)->options = xf86AddNewOption((*pInfo)->options, "driver", (*pInfo)->driver);
